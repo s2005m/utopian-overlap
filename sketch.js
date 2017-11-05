16d08ba487348d7ca713c5fb90f0c19963ca5e3b
s2005m <32396330+s2005m@users.noreply.github.com>
2017-11-05 13:15:19 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+var p;
+function setup() {
+	createCanvas(600, 400);
+	p=createElement('p','your score will be here');
+}
+var c1;
+var c2;
+
+function draw() {
+	background(255,255,0);
+	fill(255,0,0);
+	ellipse(400,200,300,300);
+	fill(0,255,0);
+	c1 = mouseX+30*0.8509035;
+	c2 = mouseY-30*0.8509035;
+	ellipse(c1,c2,60,60);
+
+}
+
+var best_score = 1000;
+function mouseClicked(){
+ var score = floor(dist(400,200,c1,c2)*100);
+ if (score<best_score){best_score=score}
+ p.html("Your score is " + score +" and Your BEST score is " + best_score);
+
+
+}
