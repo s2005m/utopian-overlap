771090f84df780c96e26504f6a4d0804cccd2d4f
Jett <whackashoe@gmail.com>
2017-11-08 01:20:30 -0600

    make the ball move

--- sketch.js
+++ sketch.js
@@ -3,13 +3,17 @@ function setup() {
 	createCanvas(600, 400);
 	p=createElement('p','your score will be here');
 }
+var bx=400;
+var by=200;
 var c1;
 var c2;
 
 function draw() {
+    bx = abs(sin(frameCount / 400)) * width;
+
 	background(255,255,0);
 	fill(255,0,0);
-	ellipse(400,200,300,300);
+	ellipse(bx,by,300,300);
 	fill(0,255,0);
 	c1 = mouseX+30*0.8509035;
 	c2 = mouseY-30*0.8509035;
@@ -19,7 +23,7 @@ function draw() {
 
 var best_score = 1000;
 function mouseClicked(){
- var score = floor(dist(400,200,c1,c2)*100);
+ var score = floor(dist(bx,by,c1,c2)*100);
  if (score<best_score){best_score=score}
  p.html("Your score is " + score +" and Your BEST score is " + best_score);
 
